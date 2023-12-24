9eda786be36b3f5174a3f6eb99eb520b4ca40fbc
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-12-24 20:28:53 +0330

    add raise value error to encoding methods

--- env/mec_offloaing_envs/offloading_env.py
+++ env/mec_offloaing_envs/offloading_env.py
@@ -246,6 +246,8 @@ class OffloadingEnvironment(MetaEnv):
                 task_encode = np.array(task_graph.encode_point_sequence_with_ranking_and_cost(scheduling_sequence, self.resource_cluster, normalize=False), dtype=np.float32)
             elif encoding == 'rank_cost_norm':
                 task_encode = np.array(task_graph.encode_point_sequence_with_ranking_and_cost(scheduling_sequence, self.resource_cluster, normalize=True), dtype=np.float32)
+            else:
+                raise ValueError("Encoding method {} is not supported".format(encoding))
             adjacency_matrix = task_graph.dependency
             adjacency_matrix = adjacency_matrix.astype(np.float32)
             permutation = np.eye(adjacency_matrix.shape[0], dtype=np.float32)[scheduling_sequence]
